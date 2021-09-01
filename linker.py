b04b7ff6c1161473077ea881d098ec512add850c
CodeDecodeDev <codedecode.dev@gmail.com>
2021-09-01 22:37:16 +0530

    changes

--- linker.py
+++ linker.py
@@ -20,7 +20,7 @@ if os.path.isfile("ez.js"):
         print(f"{args[1]} File found")
         if not os.path.exists("../FrontEnd/blogs"):
             print("Out dir not found, making one")
-            os.makedirs("../FrontEnd/blogs/")
+            os.makedirs(".../FrontEnd/blogs/")
         else: print("Out dir found")
 
         if needs_css:
@@ -33,11 +33,11 @@ if os.path.isfile("ez.js"):
         output = tools.get_data(args[1], js, css)
 
         ez_js = open("ez.js", 'r').read()
-        open("../FrontEnd/blogs/ez.js", "w").write(ez_js)
+        open(".../FrontEnd/blogs/ez.js", "w").write(ez_js)
 
         if args[1].endswith(".ez"):
             out_file_name = os.path.basename(args[1])[:-3]
         else: out_file_name = os.path.basename(args[1])
-        open(f"../FrontEnd/blogs/{out_file_name}.html", 'w').write(output)
+        open(f".../FrontEnd/blogs/{out_file_name}.html", 'w').write(output)
         print("Linking Job Done")
         print("-"*50)
